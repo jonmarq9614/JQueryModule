f93f7262435e3e1a6438c61ea9032de5509829a9
Jonathan Marquez <jmarqu12@icloud.com>
2021-05-15 01:30:06 -0600

    module 1

---
+++ MyGram2/wwwroot/lib/jquery/dist/MyGramJQuery.js
@@ -0,0 +1,29 @@
+﻿
+var h1 = $("#MyDiv");
+
+var span = $("<span></span>").text("My Gram");
+h1.append(span);
+
+var button = $("<button></button>").text("Log In");
+h1.append(button);
+
+var button2 = $("<button></button>").text("Sign Up");
+h1.append(button2);
+
+var lineBreak = $("<br>");
+h1.append(lineBreak);
+
+var image = $("<span></span>").text("Image 1 Image 2 Image 3 Image 4 Image 5");
+h1.append(image);
+
+var lineBreak2 = $("<br>");
+h1.append(lineBreak2);
+
+var aboutSection = $("<span></span>").text("About Blog Jobs Help");
+h1.append(aboutSection);
+
+var lineBreak3 = $("<br>");
+h1.append(lineBreak3);
+
+var englishSection = $("<span></span>").text("English 2021 MyGram");
+h1.append(englishSection);
