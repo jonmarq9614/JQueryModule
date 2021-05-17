5a557da589a4006c8cbfa828c492bcaf156f38ea
Jonathan Marquez <jmarqu12@icloud.com>
2021-05-16 23:16:36 -0600

    module 2

--- MyGram2/wwwroot/lib/jquery/dist/MyGramJQuery.js
+++ MyGram2/wwwroot/lib/jquery/dist/MyGramJQuery.js
@@ -4,10 +4,10 @@ var h1 = $("#MyDiv");
 var span = $("<span></span>").text("My Gram");
 h1.append(span);
 
-var button = $("<button></button>").text("Log In");
+var button = $("<button></button>").attr("id", "LogInButton").text("Log In");
 h1.append(button);
 
-var button2 = $("<button></button>").text("Sign Up");
+var button2 = $("<button></button>").attr("id", "SignUpButton").text("Sign Up");
 h1.append(button2);
 
 var lineBreak = $("<br>");
@@ -27,3 +27,25 @@ h1.append(lineBreak3);
 
 var englishSection = $("<span></span>").text("English 2021 MyGram");
 h1.append(englishSection);
+
+var lineBreak4 = $("<br>");
+h1.append(lineBreak4);
+
+function SignUpFunction() {
+    var signUpSpan1 = $("<span></span>").text("User");
+    var signUpSpan2 = $("<span></span>").text("Password");
+    $("div.Divv").append(signUpSpan1);
+    $("div.Divv").append(signUpSpan2);
+}
+
+$("#SignUpButton").click(function () {
+    SignUpFunction();
+});
+
+function LogInFunction() {
+    alert("You have logged in");
+}
+
+$("#LogInButton").click(function () {
+    LogInFunction();
+});
