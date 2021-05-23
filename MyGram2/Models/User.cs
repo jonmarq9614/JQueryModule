b5e85893291bf04f18be4a67134991abd76f9b60
Jonathan Marquez <jmarqu12@icloud.com>
2021-05-22 23:38:06 -0600

    module 5

---
+++ MyGram2/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MyGram2.Models
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
